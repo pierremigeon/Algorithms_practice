c8c0dc31b87ba89b2934b52c470695ecc1676cf8
Pierre Migeon <pi3rre.migeon@gmail.com>
2023-09-08 23:44:44 -0700

    debug algorithm

--- quick_sort/QuickSort.cpp
+++ quick_sort/QuickSort.cpp
@@ -1,23 +1,22 @@
 #include "QuickSort.hpp"
 
-int	partition(int *list, int left, int right, int pivot) {
-	while ( left < right ) {
-		if ( list[left] > list[pivot] && list[right] < list[pivot] )
-			swap(&list[left], &list[right]);
-		while ( list[left] < list[pivot] )
-			++left;
-		while ( right > left && list[right] > list[pivot] )
-			--right;
+int	partition(int *list, int left, int right) {
+	int pivot = list[right];
+	--left;
+
+	for (int j = left + 1; j < right; j++ ) {
+		if ( list[j] < pivot )
+			swap(&list[++left], &list[j]);
 	}
-	swap(&list[right], &list[pivot]);
-	return pivot;
+	swap(&list[++left], &list[right]);
+	return left;
 }
 
 void	quickSort(int *list, int i, int j) {
 	int pivot;
-	if ( i > j || i == j )
-		return;
-	pivot = partition(list, i, j - 1, j);
-	quickSort(list, i, pivot - 1);
-	quickSort(list, pivot + 1, j);
+	if ( i < j ) {
+		pivot = partition(list, i, j);
+		quickSort(list, i, pivot - 1);
+		quickSort(list, pivot + 1, j);
+	}
 }
